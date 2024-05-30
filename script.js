d677a51b67c4e2e7f8ab5b43d3c8fc0c371096f8
Eric Johnson <johnsoneric586@gmail.com>
2024-05-29 22:43:05 -0400

    Did exercise #6

--- script.js
+++ script.js
@@ -87,3 +87,39 @@ const percentages = [
 ];
 
 // console.log(percentages);
+//----------------------------------------------
+// 6) Create an array containing all the neighbouring countries of a country of your choice. Choose a country which has at least 2 or 3 neighbours. Store the array into a variable called neighbours.
+
+// At some point, a new country called 'Utopia' is created in the neighbourhood of your selected country, so add it to the end of the neighbours array.
+
+// Unfortunately, after some time the new country is dissolved, so remove it from the end of the array.
+
+// If the neighbours array does not include the country 'Germany', log to the console: 'Probably not a central european country :D'.
+
+// Change the name of one of your neighbouring countries. To do that, find the index of the country in the neighbours array, and then use that index to change the array at that index position. For example, you can search for 'Sweden' in the array, and then replace it with 'Republic of Sweden'.
+
+const neighbors = [
+  'Denmark',
+  'Poland',
+  'Czech Republic',
+  'Austria',
+  'Switzerland',
+  'France',
+  'Luxembourg',
+  'Belgium',
+  'Netherlands',
+];
+
+neighbors.push('Utopia');
+
+neighbors.pop();
+
+// if (!neighbors.includes('Germany')) {
+//   console.log('Probably not a central European country.');
+// }
+
+// neighbors[neighbors.indexOf('France')] = 'Hotdog';
+
+// console.log(neighbors);
+
+//----------------------------------------------
