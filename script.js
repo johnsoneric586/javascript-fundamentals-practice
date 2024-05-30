a138615430ea6e422773569bc6aa04313aa26299
Eric Johnson <johnsoneric586@gmail.com>
2024-05-29 22:33:04 -0400

    Did exercise #5

--- script.js
+++ script.js
@@ -66,6 +66,24 @@ function describePopulation(country, population) {
 
 // console.log(describePopulation('USA', 333.3));
 // console.log(describePopulation('China', 1441));
-// console.log(describePopulation('Japan', 1441));
+// console.log(describePopulation('Japan', 125.1));
 
 //----------------------------------------------
+// 5) Create an array containing 4 population values of 4 countries of your choice. You may use the values you have been using previously. Store this array into a variable called populations.
+
+// Log to the console whether the array has 4 elements or not (true or false).
+
+// Create an array called percentages containing the percentages of the world population for these 4 population values. Use the function percentageOfWorld1 that you created earlier to compute the 4 percentage values.
+
+const populations = [333.3, 1441, 125.1, 83.8];
+
+// console.log(populations.length === 4);
+
+const percentages = [
+  percentageOfWorld1(populations[0]),
+  percentageOfWorld1(populations[1]),
+  percentageOfWorld1(populations[2]),
+  percentageOfWorld1(populations[3]),
+];
+
+// console.log(percentages);
