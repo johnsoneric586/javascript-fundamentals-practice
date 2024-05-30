13545a117791dc93ee69132d0be65d1b1af6df87
Eric Johnson <johnsoneric586@gmail.com>
2024-05-29 22:25:08 -0400

    Did exercise #4

--- script.js
+++ script.js
@@ -52,4 +52,20 @@ const percentageOfWolrd3 = population => (population / 7900) * 100;
 // console.log(percentageOfWolrd3(333.3));
 
 //----------------------------------------------
-// 4)
+// 4) Create a function called describePopulation. Use the function type you like the most. This function takes in two arguments: country and population, and returns a strings like this: 'China has 1441 million people, which is about 18.2% of the world'.
+
+// To calculate the percentage, describePopulation calls the percentageOfWorld1 you created earlier.
+
+// Call describePopulation with data for 3 countries of your choice.
+
+function describePopulation(country, population) {
+  return `${country} has ${population} million people, which is about ${percentageOfWorld1(
+    population
+  )}% of the world.`;
+}
+
+// console.log(describePopulation('USA', 333.3));
+// console.log(describePopulation('China', 1441));
+// console.log(describePopulation('Japan', 1441));
+
+//----------------------------------------------
